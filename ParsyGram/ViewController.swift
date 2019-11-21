169ae3117a42ccb157848e4bb0c181d5a294361b
rainarit <rraina@ucsd.edu>
2019-11-20 21:38:51 -0800

    Initial Commit

---
+++ ParsyGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsyGram
+//
+//  Created by Ritik Raina on 11/20/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
