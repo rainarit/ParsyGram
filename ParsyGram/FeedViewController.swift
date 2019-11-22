38f223239d404e5b2ba330c3249e6f4c79d16dc3
rainarit <rraina@ucsd.edu>
2019-11-22 01:45:18 -0800

    Fixed how new posts show up on top of the feed.

--- ParsyGram/FeedViewController.swift
+++ ParsyGram/FeedViewController.swift
@@ -30,6 +30,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let query = PFQuery(className:"Posts")
+        query.order(byDescending: "createdAt")
         query.includeKey("author")
         query.limit = 20
         
