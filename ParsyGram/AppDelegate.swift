f6324cf9cadf7d23b489460e9eb4e936132bcd9c
rainarit <rraina@ucsd.edu>
2019-11-24 15:11:02 -0800

    Assignment 6

--- ParsyGram/AppDelegate.swift
+++ ParsyGram/AppDelegate.swift
@@ -24,6 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://parsygram.herokuapp.com/parse"
             })
         )
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
+        }
         return true
     }
 
---
+++ ParsyGram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  ParsyGram
+//
+//  Created by Ritik Raina on 11/24/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
